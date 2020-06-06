af412842129ec2d5e55bca31056bc832eb05178f
ojferro <ojferro@gmail.com>
2020-06-06 00:34:52 -0700

    Add custom actions

--- nlu/actions.py
+++ nlu/actions.py
@@ -7,21 +7,99 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionRespondBye(Action):
+
+    def name(self) -> Text:
+        return "action_respond_bye"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Bye!")
+
+        return []
+
+class ActionRespondGreet(Action):
+
+    def name(self) -> Text:
+        return "action_respond_greet"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Greetings")
+
+        return []
+
+class ActionPlayMedia(Action):
+
+    def name(self) -> Text:
+        return "action_play_media"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Alright, playing music.")
+
+        return []
+
+class ActionRespondThank(Action):
+
+    def name(self) -> Text:
+        return "action_respond_thank"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="You are most welcome.")
+
+        return []
+
+class ActionRespondOutOfScope(Action):
+
+    def name(self) -> Text:
+        return "action_respond_out_of_scope"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="I am not sure how to do that, would you like to teach me? [not implemented yet]")
+
+        return []
+
+# class ActionFallback(Action):
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_fallback"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         dispatcher.utter_message(text="Did not understand you")
+
 #         return []
